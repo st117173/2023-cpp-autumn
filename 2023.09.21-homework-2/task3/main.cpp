89d3063c752c0de3c259534e2ace28f330e5b441
st117173 <st117173@student.spbu.ru>
2023-09-22 18:43:06 +0300

    task 3 create

--- 2023.09.21-homework-2/task3/main.cpp
+++ 2023.09.21-homework-2/task3/main.cpp
@@ -3,14 +3,24 @@
 int main() 
 {
 	int a = 0;
-	int b = o;
+	int b = 0;
 	int c = 0;
 	int d = 0;
-	int x = 0;
 	std::cin >> a;
 	std::cin >> b;
 	std::cin >> c;
 	std::cin >> d;
-	if ((a*x + b) / (c*x + d) = 0)
-
+	if (a == 0 && b == 0) 
+	{
+		std::cout << "INF";
+	}
+	else if (a == 0 || b % a != 0 || c * (-b / a) + d == 0) 
+	{
+		std::cout << "NO";
+	}
+	else 
+	{
+		std::cout << -b / a;
+	}
+	return 0;
 }
