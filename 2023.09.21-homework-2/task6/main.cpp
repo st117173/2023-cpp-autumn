f8565991f152231cb963f61311054922499637df
st117173 <st117173@student.spbu.ru>
2023-09-22 17:51:23 +0300

    new file

--- 2023.09.21-homework-2/task6/main.cpp
+++ 2023.09.21-homework-2/task6/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 
-int main()
-{
-	int k = 0;
-	int m = 0;
-	int n = 0;
-	std::cin >> k;
-	std::cin >> m;
-	std::cin >> n;
-	m = (k / n) * 2 * m;
-	std::cout << m;
-	return 0;
+int main() {
+    int k = 0;
+    int m = 0;
+    int n = 0;
+    int g = 0;
+    std::cin >> k;
+    std::cin >> m;
+    std::cin >> n;
+    g = (n / k) * 2 * m;
+    if (n % k > 0) 
+    {
+        if ((k / 2) < (n % k)) 
+        {
+            g = g + m * 2;
+        }
+        else 
+        {
+            g = g + m;
+        }
+    }
+    if (n <= k) 
+    {
+        g = 2 * m;
+    }
+    std::cout << g;
 
+    return 0;
 }
