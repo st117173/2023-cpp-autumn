d0b94c0d41931fdbcf59c752542b8aff57a9271c
st117173 <st117173@student.spbu.ru>
2023-09-22 19:17:41 +0300

    task 4

--- 2023.09.21-homework-2/task4/main.cpp
+++ 2023.09.21-homework-2/task4/main.cpp
@@ -1,25 +1,55 @@
-#include <iosream>
+#include <iostream>
 
-int main() 
+int main()
 {
-	int n = 0;
-	int a = 0;
-	std::cin >> n;
-	if (n == 1){
-		std::cout << 15;
-	}
-	else if (n == 5) {
-		std::cout << 70;
-	}
-	else if (n == 10) {
-		std::cout << 125;
-	}
-	else if (n == 20) {
-		std::cout << 230;
-	}
-	else if (n == 30) {
-		std::cout << 440;
-	}
+    int t1 = 15;
+    int t5 = 70;
+    int t10 = 125;
+    int t20 = 230;
+    int t60 = 440;
+    int n = 0;
+    int g1 = 0;
+    int g5 = 0;
+    int g10 = 0;
+    int g20 = 0;
+    int g60 = 0;
 
+    std::cin >> n;
 
+    g60 = n / 60;
+    n %= 60;
+    g20 = n / 20;
+    n %= 20;
+    g10 = n / 10;
+    n %= 10;
+    g5 = n / 5;
+    g1 = n % 5;
+    if (g1 * t1 >= t5)
+    {
+        g1 = 0;
+        g5++;
+    }
+    if (g1 * t1 + g5 * t5 >= t10)
+    {
+        g1 = 0;
+        g5 = 0;
+        g10++;
+    }
+    if (g1 * t1 + g5 * t5 + g10 * t10 >= t20)
+    {
+        g1 = 0;
+        g5 = 0;
+        g10 = 0;
+        g20++;
+    }
+    if (g1 * t1 + g5 * t5 + g10 * t10 + g20 * t20 >= t60)
+    {
+        g1 = 0;
+        g5 = 0;
+        g10 = 0;
+        g20 = 0;
+        g60++;
+    }
+    std::cout << g1 << " " << g5 << " " << g10 << " " << g20 << " " << g60;
+    return 0;
 }
