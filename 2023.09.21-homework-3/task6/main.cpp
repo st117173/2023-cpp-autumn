427a2f02b89c4eec2cca5ecc6bd6ece285b38e59
st117173 <st117173@student.spbu.ru>
2023-09-29 18:00:03 +0300

    create task6

--- 2023.09.21-homework-3/task6/main.cpp
+++ 2023.09.21-homework-3/task6/main.cpp
@@ -2,18 +2,27 @@
 
 int main()
 {
-	int n = 0;
-	int a1 = 0;
-	int a2 = 1;
-	int b = 0;
-	std::cin >> n;
-	while (n > 0)
-	{
-		b = a2;
-		a2 = a1 + a2;
-		a1 = b;
-		n = n - 1;
-	}
+    int a1 = 0;
+    int a2 = 1;
+    int a3 = 0;
+    int n = 0;
+    std::cin >> n;
 
-
+    if (n == 1)
+    {
+        std::cout << a2;
+    }
+    else
+    {
+        int i = 2;
+        while (i <= n)
+        {
+            a3 = a1 + a2;
+            a1 = a2;
+            a2 = a3;
+            i++;
+        }
+        std::cout << a2;
+    }
+    return 0;
 }
