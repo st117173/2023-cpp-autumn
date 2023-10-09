bd80538ed223f61a6ad704fc15ddb5ca97facaf9
Evdokiya <st117173@student.spbu.ru>
2023-10-09 15:14:21 +0300

    create task4

---
+++ 2023.09.21-homework-4/task4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int, char**)
+{
+	int n = 0;
+	std::cin >> n;
+	int max = 0;
+	int* a = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> *(a + i);
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		if (max < *(a + i))
+		{
+			max = *(a + i);
+		}
+	}
+	std::cout << max;
+	free(a);
+
+	return EXIT_SUCCESS;
+}
