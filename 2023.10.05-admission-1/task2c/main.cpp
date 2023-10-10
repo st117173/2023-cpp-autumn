ea79171a00ac8876508c9c8f89d48deb06d4d7e1
Evdokiya <st117173@student.spbu.ru>
2023-10-10 21:59:45 +0300

    create task2c

--- 2023.10.05-admission-1/task2c/main.cpp
+++ 2023.10.05-admission-1/task2c/main.cpp
@@ -5,11 +5,11 @@ int main(int argc, char* argv[])
 	int n = 0;
 	std::cin >> n;
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 1; i <= n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
 		{
-			std::cout << j + i;
+			std::cout << (i + j) % (n + 1) + (i + j) / (n + 1);
 		}
 		std::cout << std::endl;
 	}
