1924a846025fe838aac5c171bf9f07355020fe31
Evdokiya <st117173@student.spbu.ru>
2023-10-12 22:46:12 +0300

    create task2

---
+++ 2023.10.05-admission-2/task2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	std::cin >> n;
+
+	if (n > 0)
+	{
+		for (int i = 0; i <= n; ++i)
+		{
+			std::cout << i << " ";
+		}
+		for (int i = n - 1; i >= 0; --i)
+		{
+			std::cout << i << " ";
+		}
+	}
+	else if (n < 0)
+	{
+		for (int i = 0; i >= n; --i)
+		{
+			std::cout << i << " ";
+		}
+		for (int i = n + 1; i <= 0; ++i)
+		{
+			std::cout << i << " ";
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
